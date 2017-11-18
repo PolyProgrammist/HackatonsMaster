5a032fa9187ce887006b561cc9707ca6122d9cd8
polyprogrammist <pechkin350@gmail.com>
2017-11-18 20:04:00 +0300

    lol

---
+++ PyQuery/GitHubCommits.py
@@ -0,0 +1,9 @@
+import requests
+
+class GitHubRequester:
+    def get_new_commits(self):
+        pass
+    def _get_all_commits(self):
+        pass
+    def _get_all_branches(self):
+        pass
